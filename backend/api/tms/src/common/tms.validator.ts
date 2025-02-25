a95a8fb1eae8439319f6e379b3bdf4fa9948f2d1
Shankar Sethuraman <shankar.sethuraman@aot-technologies.com>
2025-02-25 09:58:44 -0700

    added more validations and updated readme for docker-compose

--- backend/api/tms/src/common/tms.validator.ts
+++ backend/api/tms/src/common/tms.validator.ts
@@ -1,5 +1,4 @@
 import { Joi } from 'express-validation'
-import { TMSConstants } from './tms.constants'
 
 export default {
 
@@ -31,7 +30,31 @@ export default {
                 ssoUserId: Joi.string().required(), // will need to be updated to the right regex and length
                 email:Joi.string().regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/).max(100).required(),                
             }).min(1)
-        })
+        }).options({abortEarly:false,convert:false})
+    },
+
+    getUserTenants: {
+        params: Joi.object({
+            ssoUserId: Joi.string().min(2).required()
+        }).options({abortEarly:false,convert:false})
+    },
+
+    getTenantUsers: {
+        params: Joi.object({
+            id: Joi.string().guid().required()
+        }).options({abortEarly:false,convert:false})
+    },
+
+    createTenantRoles: {
+        params: Joi.object({
+            id: Joi.string().guid().required()
+        }),
+        body: Joi.object({
+            role: Joi.object().keys({
+                name: Joi.string().min(1).max(100).required(),
+                description: Joi.string().min(1).max(255).required()
+            }).min(1)
+        }).options({abortEarly:false,convert:false})
     }
 
 }
