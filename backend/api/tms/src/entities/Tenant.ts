d1a2fd1419862394d1de11d93db54f0df5b5c8c9
Shankar Sethuraman <shankar.sethuraman@aot-technologies.com>
2025-03-07 12:59:40 -0700

    updated unique constraints - tenant name duplicable, tenant name ministry name combo is not

--- backend/api/tms/src/entities/Tenant.ts
+++ backend/api/tms/src/entities/Tenant.ts
@@ -1,14 +1,15 @@
-import { Entity, PrimaryGeneratedColumn, Column, Timestamp, CreateDateColumn, UpdateDateColumn, OneToMany, DefaultNamingStrategy } from 'typeorm'
+import { Entity, PrimaryGeneratedColumn, Column, Timestamp, CreateDateColumn, UpdateDateColumn, OneToMany, Unique } from 'typeorm'
 import { TenantUser } from './TenantUser'
 import { Role } from './Role'
 
 @Entity('Tenant')
+@Unique(["name", "ministryName"])
 export class Tenant {
 
       @PrimaryGeneratedColumn('uuid', { name: 'id' })
       id:string
 
-      @Column({length:30, name:'name', unique: true})
+      @Column({length:30, name:'name'})
       name:string
 
       @Column({length:100, name:'ministry_name'})
