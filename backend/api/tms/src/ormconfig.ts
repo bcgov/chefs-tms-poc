35cd6f057a2ec8e5e0b88dafd0b7d7b44f870617
Shankar Sethuraman <shankar.sethuraman@aot-technologies.com>
2025-03-17 15:48:04 -0600

    orm config clean up to dynamically point to actual database vs testcontainer

--- backend/api/tms/src/ormconfig.ts
+++ backend/api/tms/src/ormconfig.ts
@@ -2,63 +2,30 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const isTestEnv = process.env.NODE_ENV === "test"
- let dbConfig
-
-if(isTestEnv) {
-  dbConfig = {
-    type: 'postgres',
-    host: 'localhost',
-    port: 54321,
-    username: 'testuser',
-    password: 'testpassword',
-    database: 'testdb',
-    synchronize: false,
-    logging: true,
-    entities: [
-      'src/entities/**/*.ts'
-    ],
-    migrations: [
-      './src/migrations/*.ts'
-    ],
-    subscribers: [
-      'src/subscriber/**/*.ts'
-    ],
-    cli: {
-      entitiesDir: 'src/entities',
-      migrationsDir: 'src/migration',
-      subscribersDir: 'src/subscriber'
-    }
- }
-}
-
- else {
-
-  dbConfig = {
-    type: 'postgres',
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    synchronize: false,
-    logging: false,
-    entities: [
-      'src/entities/**/*.ts'
-    ],
-    migrations: [
-      './src/migrations/*.ts'
-    ],
-    subscribers: [
-      'src/subscriber/**/*.ts'
-    ],
-    cli: {
-      entitiesDir: 'src/entities',
-      migrationsDir: 'src/migration',
-      subscribersDir: 'src/subscriber'
-    }
- }
-
- }
  
+  const dbConfig = {
+    type: 'postgres',
+    host: isTestEnv? 'localhost' : process.env.DB_HOST,
+    port: isTestEnv? 54321: process.env.DB_PORT,
+    username: isTestEnv? 'testuser': process.env.DB_USER,
+    password: isTestEnv? 'testpassword': process.env.DB_PASSWORD,
+    database: isTestEnv? 'testdb': process.env.DB_DATABASE,
+    synchronize: false,
+    logging: isTestEnv,
+    entities: [
+      'src/entities/**/*.ts'
+    ],
+    migrations: [
+      './src/migrations/*.ts'
+    ],
+    subscribers: [
+      'src/subscriber/**/*.ts'
+    ],
+    cli: {
+      entitiesDir: 'src/entities',
+      migrationsDir: 'src/migration',
+      subscribersDir: 'src/subscriber'
+    }
+ }
 
 module.exports = dbConfig
